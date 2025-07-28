f05091afe4763729c44e4ae4ef523a56cadb685b
Psaivenkat <psvenkat3113@gmail.com>
2025-07-28 22:58:02 +0530

    NavBar and HeroBanner

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,8 +1,20 @@
 // pages/Home.js
-import React from "react";
+import React, { useState } from "react";
+import { Box } from "@mui/material";
+
+import HeroBanner from "../Components/HeroBanner";
+import SearchExercises from "../Components/SearchExercises";  
+import Exercises from "../Components/Exercises";
 
 const Home = () => {
-  return <div>Home Page</div>;
+  return (
+    <Box>
+      <HeroBanner />
+      <SearchExercises />
+      <Exercises />
+    </Box>
+
+  )
 };
 
 export default Home;
